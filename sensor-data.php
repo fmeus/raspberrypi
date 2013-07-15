e6c9782502de3562e81fe5097aad839e9d3cefc9
Frank Meus <frank.meus@gmail.com>
2013-07-15 11:56:22 +0200

    Initial commit

---
+++ sensor-data.php
@@ -0,0 +1,230 @@
+<?php
+/* ============================================================================================== */
+/* SQLite tables                                                                                  */
+/* ============================================================================================== */
+    define( 'NEWLINE', "\n");
+
+/* ============================================================================================== */
+/* SQLite tables                                                                                  */
+/* ============================================================================================== */
+    $tables = array( 'sensor_data'
+                   , 'sensors' 
+                   );
+
+
+/* ============================================================================================== */
+/* Open SQLite connection                                                                         */
+/* ============================================================================================== */
+    $db = new SQLite3( 'sensor-data.sqlite' );
+
+
+/* ============================================================================================== */
+/* Prepared SQLite statements                                                                     */
+/* ============================================================================================== */
+    $stmt_sensor_data = new StdClass();
+    $stmt_sensors = new StdClass();
+
+    function process_prepare_statements() {
+        global $db, $stmt_sensor_data, $stmt_sensors;
+
+        if ( $db->busyTimeout( 2000 ) ) {
+            $stmt_sensors = $db->prepare( 'insert into sensors( sensor_type
+                                                              , sensor_name
+                                                              , sensor_location )
+                                                        values( :sensor_type
+                                                              , :sensor_name
+                                                              , :sensor_location )' );
+
+            $stmt_sensor_data = $db->prepare( 'insert into sensor_data( sensor_id
+                                                                      , value )
+                                                                values( :sensor_id
+                                                                      , :value )' );
+        }
+        $db->busyTimeout( 0 );
+    }
+
+
+/* ============================================================================================== */
+/* Execute SQLite statement with wait for unlocked state (default 10.000 ms                       */
+/* ============================================================================================== */
+    function execute_timeout( $statement, $timeout = 10000 ) {
+        global $db;
+
+        if ( $db->busyTimeout( $timeout ) ) {
+            $statement->execute();
+        }
+
+        $db->busyTimeout( 0 );
+    }
+
+
+/* ============================================================================================== */
+/* Perform SQLite query with wait for unlocked state (default 2000ms)                             */
+/* ============================================================================================== */
+    function query_timeout( $query, $timeout = 6000 ) {
+        global $db;
+
+        if ( $db->busyTimeout( $timeout ) ) {
+            $results = $db->query( $query );
+        }
+
+        $db->busyTimeout( 0 );
+
+        return $results;
+    }
+
+
+/* ============================================================================================== */
+/* Add sensor                                                                                     */
+/* ============================================================================================== */
+    function process_add_sensor( $type, $name, $location ) {
+        global $db, $stmt_sensors;
+
+        $stmt_sensors->bindValue( ':sensor_type', $type, SQLITE3_TEXT );
+        $stmt_sensors->bindValue( ':sensor_name', $name, SQLITE3_TEXT );
+        $stmt_sensors->bindValue( ':sensor_location', $location, SQLITE3_TEXT );
+        execute_timeout( $stmt_sensors );
+    }
+
+/* ============================================================================================== */
+/* List sensors                                                                                   */
+/* ============================================================================================== */
+    function process_list_sensors() {
+        $results = query_timeout( "select sensor_id, sensor_type, sensor_name, sensor_location from sensors" );
+
+        echo str_pad( 'ID', 5, ' ', STR_PAD_LEFT) ;
+        echo ' ';
+        echo str_pad( 'TYPE', 12, ' ', STR_PAD_RIGHT) ;
+        echo ' ';
+        echo str_pad( 'NAME', 25, ' ', STR_PAD_RIGHT) ;
+        echo ' ';
+        echo str_pad( 'LOCATION', 25, ' ', STR_PAD_RIGHT).NEWLINE;
+        echo ' ';
+        echo str_pad( '', 67, '-', STR_PAD_LEFT).NEWLINE;
+
+        while ( $row = $results->fetchArray( SQLITE3_ASSOC ) ) {
+            echo str_pad( $row['sensor_id'], 5, ' ', STR_PAD_LEFT);
+            echo ' ';
+            echo str_pad( $row['sensor_type'], 12, ' ', STR_PAD_RIGHT);
+            echo ' ';
+            echo str_pad( $row['sensor_name'], 25, ' ', STR_PAD_RIGHT);
+            echo ' ';
+            echo str_pad( $row['sensor_location'], 25, ' ', STR_PAD_RIGHT).NEWLINE;
+        }
+    }
+
+
+/* ============================================================================================== */
+/* Log sensor data                                                                                */
+/* ============================================================================================== */
+    function process_log_data( $id, $value ) {
+        global $db, $stmt_sensor_data;
+
+        $stmt_sensor_data->bindValue( ':sensor_id', $type, SQLITE3_INTEGER );
+        $stmt_sensor_data->bindValue( ':value', $name, SQLITE3_REAL );
+        execute_timeout( $stmt_sensor_data );
+    }
+
+
+/* ============================================================================================== */
+/* Create SQLite tables                                                                           */
+/* ============================================================================================== */
+    function process_create_tables() {
+        global $db;
+
+        $db->exec( 'create table sensors( sensor_id        integer primary key autoincrement
+                                        , sensor_type      text check( sensor_type in (\'TEMPERATURE\',\'HUMIDITY\',\'MOTION\',\'LIGHT\') ) not null default \'\'
+                                        , sensor_name      text not null
+                                        , sensor_location  text not null )' );
+
+        $db->exec( 'create table sensor_data( id           integer primary key autoincrement
+                                            , timestamp    datetime default current_timestamp
+                                            , sensor_id    integer not null
+                                            , value        real )' );
+    }
+
+
+/* ============================================================================================== */
+/* Drop SQLite tables                                                                             */
+/* ============================================================================================== */
+    function process_drop_tables() {
+        global $db, $tables;
+
+        foreach ( $tables as $table ) {
+            $db->exec( "drop table $table" );
+        }
+    }
+
+
+/* ============================================================================================== */
+/* Empty SQLite tables                                                                            */
+/* ============================================================================================== */
+    function process_empty_tables() {
+        global $db, $tables;
+
+        foreach ( $tables as $table ) {
+            $db->exec( "delete from $table" );
+        }
+    }
+
+
+/* ============================================================================================== */
+/* Main process                                                                                   */
+/* ============================================================================================== */
+    $command = isset( $argv[1] )? $argv[1] : $_GET['action'];
+
+    switch( $command )
+    {
+        case 'add_sensor';
+            process_prepare_statements();
+            process_add_sensor( 'TEMPERATURE', 'DS18B20', 'Office' );
+            process_add_sensor( 'TEMPERATURE', 'DHT22', 'Office' );
+            process_add_sensor( 'HUMIDITY', 'DHT22', 'Office' );
+        break;
+
+        case 'list_sensors';
+            process_list_sensors();
+        break;
+
+        case 'log_data';
+            process_prepare_statements();
+            $id = $_GET['id'];
+            $id = $_GET['value'];
+            process_log_data( $id, $value );
+        break;
+
+        case 'drop';
+            process_drop_tables();
+        break;
+
+        case 'create';
+            process_create_tables();
+        break;
+
+        case 'empty';
+            process_empty_tables();
+        break;
+
+        case 'vacuum';
+            $db->exec( 'vacuum' );
+        break;
+
+        case 'cleanup';
+            process_cleanup();
+        break;
+
+        default;
+            echo "No command or invalid command was specified. Known commands are;\n";
+            echo "   drop    - Drop SQLite tables\n";
+            echo "   create  - Create SQLite tables\n";
+            echo "   empty   - Empty SQLite tables\n";
+            echo "   vacuum  - Rebuild entire database\n";
+        break;
+    }
+
+
+/* ============================================================================================== */
+/* Close SQLite connection                                                                        */
+/* ============================================================================================== */
+    $db->close();
+?>
