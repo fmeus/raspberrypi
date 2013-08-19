a86fa0ea45c6e41a4714180b1a97fd6243ef7edd
Frank Meus <frank.meus@gmail.com>
2013-08-19 11:34:26 +0200

    Initial commit

---
+++ Rules.php
@@ -0,0 +1,100 @@
+<?php
+class Rules
+{
+	/* Connection to MySQL database */
+	private $connection;
+
+	/* Private properties (exposed via Getter) */
+	private $_description;
+	private $_preprocess;
+	private $_postprocess;
+	private $_query;
+	private $_message;
+	private $_output;
+
+
+	private function connect( $host, $username, $password, $database )
+	{
+		$this->connection = new mysqli( $host, $username, $password, $database );
+	}
+
+
+	private function disconnect()
+	{
+		$this->connection->close();
+	}
+
+
+	/* Get rule data */
+	private function get_rule_data( $ruleid )
+	{
+		if ( $results = $this->connection->query( "select rule_desc
+			                                       ,      rule_query
+			                                       ,      rule_message
+			                                       ,      rule_active
+			                                       ,      rule_preproc
+			                                       ,      rule_postproc
+			                                       from   rules
+			                                       where  rule_id = ${ruleid}" ) ) {
+			$row = mysqli_fetch_array( $results );
+			$this->_description = $row[0];
+			$this->_query = $row[1];
+			$this->_message = $row[2];
+			$this->_active = $row[3];
+			$this->_preprocess = $row[4];
+			$this->_postprocess = $row[5];
+		}
+	}
+
+
+	public function run_rule( $ruleid )
+	{
+		/* Get rule data */
+		$this->get_rule_data( $ruleid );
+
+		if ( $this->active == 'Y' ) {
+			/* Clear any previous output */
+			$this->_output = null;
+
+			/* Execute pre process */
+			$this->connection->query( $this->_preprocess );
+
+			/* Execute the actual rule */
+			$rule_result = $this->connection->query( $this->_preprocess );
+
+			/* Format the message */
+			if ( $rule_result ) {
+				$this->_output = sprintf( $this->_message, mysqli_fetch_array( $rule_result, MYSQLI_NUM ) );
+			}
+
+			/* Execute post process */
+			$this->connection->query( $this->_postprocess );
+
+			/* Update last usage timestamp for rule */
+			$this->connection->query( "update rules set rule_last_used=now() where rule_id = ${rule_id}" );
+
+			/* Result result message */
+			return ( strlen( $this->_output ) > 0 );
+		} 
+
+		/* Return failed indicator */
+		return false;
+	}
+
+
+	/* Default Getter */
+	public function __get( $property ) {
+		$property = '_'.strtolower( $property );
+		if ( property_exists( $this, $property ) ) {
+			return $this->$property;
+		}
+	}
+
+
+	/* Default Constructor */
+	public function __construct( $host, $username, $password, $database )
+	{
+		$this->connect( $host, $username, $password, $database );
+	}
+}
+?>
