29cc2a67c4af0d56203504b1a97f39b38666d43c
Frank Meus <frank.meus@gmail.com>
2013-08-20 23:28:24 +0200

    Test scripts for rules and pushover

---
+++ test_rule.php
@@ -0,0 +1,11 @@
+<?php
+require_once( 'Rules.php' );
+require_once( 'pushover.php' );
+
+
+$rf = new Rules( 'localhost', 'rpi', 'rpi', 'sensordata' );
+$rf->run_rule( 1 );
+
+// echo $rf->getOutput();
+send_notification( $rf->getDescription(), $rf->getOutput() );
+?>
---
+++ test_rule.py
@@ -0,0 +1,6 @@
+import pushover
+from Rules import Rules
+
+rf = Rules( 'localhost', 'rpi', 'rpi', 'sensordata' )
+rf.run_rule( 1 )
+pushover.send_notification( rf.getDescription(), rf.getOutput() )
