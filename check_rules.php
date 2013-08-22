4d2a6685f98a81b15e7a7e440b8b589b5c5674e3
Frank Meus <frank.meus@gmail.com>
2013-08-22 13:53:40 +0200

    Simplified executing rules

--- check_rules.php
+++ check_rules.php
@@ -5,25 +5,16 @@
 
 	define( 'URL', 'http://littlegemsoftware.com:314/chart-sensor.html' );
 	define( 'URL_TITLE', 'RPi Sensor Data' );
+	define( 'RULES_TO_RUN', serialize( array( 2, 3, 4 ) ) );
 
 	/* Setup Rules */
 	$rf = new Rules( 'localhost', 'rpi', 'rpi', 'sensordata' );
 
-	/* Run rule - New Record */
-	$rf->run_rule( 2 );
-	if ( strlen( $rf->getOutput() ) > 0 ) {
-		send_notification( $rf->getDescription(), $rf->getOutput(), URL, URL_TITLE );
-	}
-
-	/* Run rule - No Data Logged */
-	$rf->run_rule( 3 );
-	if ( strlen( $rf->getOutput() ) > 0 ) {
-		send_notification( $rf->getDescription(), $rf->getOutput(), URL, URL_TITLE );
-	}
-
-	/* Run rule - Unhealthy Humidity */
-	$rf->run_rule( 4 );
-	if ( strlen( $rf->getOutput() ) > 0 ) {
-		send_notification( $rf->getDescription(), $rf->getOutput(), URL, URL_TITLE );
+	/* Loop through rules */
+	foreach( unserialize( RULES_TO_RUN ) as $ruleid ) {
+		$rf->run_rule( $ruleid );
+		if ( strlen( $rf->getOutput() ) > 0 ) {
+			send_notification( $rf->getDescription(), $rf->getOutput(), URL, URL_TITLE );
+		}
 	}
 ?>
