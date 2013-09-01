4fca496c26bc3f568c62332d8a81e9a16fc0a008
fmeus <frank.meus@gmail.com>
2013-09-01 11:51:05 +0200

    Updated rule message

--- rules.sql
+++ rules.sql
@@ -5,7 +5,7 @@ insert into rules (rule_desc, rule_query, rule_message, rule_active) values( 'Te
 insert into rules(rule_desc,rule_query,rule_message,rule_preproc,rule_postproc,rule_active) values('New Record','select sen.sensor_name, new.min_value, new.max_value from sensor_high_low old, tmp_high_low new, sensors sen where old.sensor_id = new.sensor_id and old.sensor_id = sen.sensor_id and ( old.min_value > new.min_value or old.max_value < new.max_value );','New record for %s, Low = %.2f / High = %.2f', 'call new_high_low()','call update_high_low()','Y');
 
 -- Rule: No Data Logged
-insert into rules (rule_desc, rule_query, rule_message, rule_active) values('No Data Logged', 'select date_format( x.ts, \'%M %d, %Y at %H:%i:%s\' ) from ( select max(timestamp) as ts from sensor_data ) x where timestampdiff( MINUTE, x.ts, now() ) > 5;', 'No data has been logged for 5 minutes now. Last entry was on %s', 'Y');
+insert into rules (rule_desc, rule_query, rule_message, rule_active) values('No Data Logged', 'select date_format( x.ts, \'%M %d, %Y at %H:%i:%s\' ) from ( select max(timestamp) as ts from sensor_data ) x where timestampdiff( MINUTE, x.ts, now() ) > 5;', 'No data has been logged for at least 5 minutes. Last entry was on %s', 'Y');
 
 -- Rule: Unhealthy Humidity
 insert into rules (rule_desc, rule_query, rule_message, rule_active) values('Unhealthy Humidity', 'select d.value from sensor_data d, (select max(id) as id from sensor_data where sensor_id = 3) c where d.id = c.id and ( d.value < 30 or d.value > 60 );', 'Humidity is outside the healthy range (30-60%%). Humidity is %.2f%%', 'Y');
