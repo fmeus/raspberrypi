4118b9be0fbc3d2f1f3c807ff4ec360459d7897d
fmeus <frank.meus@gmail.com>
2013-09-19 02:23:56 +0200

    Additional instructions related to specific raspbian revision

--- install-tl-wn725n.sh
+++ install-tl-wn725n.sh
@@ -20,3 +20,34 @@ sudo modprobe 8188eu
 #         wpa-ssid "your-ssid"
 #         wpa-psk "your-password"
 # iface default inet dhcp
+
+
+#
+# http://www.raspberrypi.org/phpBB3/viewtopic.php?t=55779&p=422649
+#
+# by MrEngman » Mon Sep 16, 2013 5:38 pm
+# wewa wrote:
+# I've also tried this but it also produces an error.
+#
+# 	user@raspberrypi / $ sudo insmod /lib/modules/3.6.11+/kernel/drivers/net/wireless/8188eu.ko
+# 	Error: could not insert module /lib/modules/3.6.11+/kernel/drivers/net/wireless/8188eu.ko: Invalid module format
+#
+# I have loaded an SD card with 2013-07-26-wheezy-raspbian and installed the same driver you used and it loads and the wifi works OK. Have you updated you image using apt-get update/upgrade or rpi-update? You shouldn't get that error unless your kernel revision has changed.
+#
+# I have three different driver revisions. Which one you should use depends on the revision of raspbian you have installed. This you can find using command uname -a
+#
+# After running sudo rpi-update my own RPi shows
+#
+# 	Linux raspberrypi 3.6.11+ #541 PREEMPT Sat Sep 7 19:46:21 BST 2013 armv6l GNU/Linux
+#
+# for 3.6.11+ #538 and #541 use 8188eu-20130830.tar.gz
+# for 3.6.11+ #524, #528 or #532 use 8188eu-20130815.tar.gz
+# for 3.6.11+ #371 up to #520 use 8188eu-20130209.tar.gz
+#
+# Download and install the driver using the commands
+#
+# 	wget https://dl.dropboxusercontent.com/u/80256631/8188eu-2013xxyy.tar.gz  <--set data code for driver version
+# 	tar -zxvf 8188eu-2013xxyy.tar.gz                                          <--set data code for driver version
+# 	sudo install -p -m 644 8188eu.ko /lib/modules/3.6.11+/kernel/drivers/net/wireless
+# 	sudo insmod /lib/modules/3.6.11+/kernel/drivers/net/wireless/8188eu.ko
+# 	sudo depmod -a
